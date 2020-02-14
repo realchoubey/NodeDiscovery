879579de8e51e2bc4dcafdda92d7cbce645d2bbb
Vikash Kumar Choubey <Vikash.2488@gmail.com>
2020-02-14 12:12:48 +0530

    File reading.

--- SendAndReceive.py
+++ SendAndReceive.py
@@ -1,10 +1,36 @@
 import socket
 import signal
 
+class machineDetails():
+	def __init__(self, machine_name="", machine_ip=""):
+		self.populate_details()
+
+	def populate_details():
+		with open("configuration") as conf_file:
+			# Get Machine name
+			self.machine_name = fp.readline()
+
+			# Get Mahine IP
+			self.machine_ip = fp.readline()
+
+			# Get neigbhor details
+			neigbhor_details = fp.readline()
+
+		# TODO confirm input are correct from file.
+		self.populate_neigbhor(neigbhor_details)
+
+	def populate_neigbhor(self, neigbhor_details):
+		neigbhor_ip_port_list = neigbhor_details.split(',')
+		for ip_port in neigbhor_ip_port_list:
+			neigbhor_ip_port = ip_port.split(':')
+			self.list_neigbhors.append(neigbhor_ip_port)
+
+
 class socketConnection():
 	def __init__(self, port=80):
 		self.socket_conn = socket.socket()
 		self.port = port
+		self.list_neigbhors_
 
 	def bind_connection(self, host_name=''):
 		self.socket_conn.bind(host_name, self.port)
@@ -38,6 +64,7 @@ class socketConnection():
 	def close_connections(self):
 		self.current_neigbhor_conn.close()
 		self.socket_conn.close()
+
 
 
 if __name__ == '__main__':
